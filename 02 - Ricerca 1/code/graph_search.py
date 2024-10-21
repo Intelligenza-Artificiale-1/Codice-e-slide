0900d30ef523749deaf3d6557873a545c54dc6fc
Andrea-Augello <andrea.augello01community.unipa.it>
2024-10-21 08:52:57 +0200

    Caricato materiale esercitazione 21/10/2024

--- 02 - Ricerca 1/code/graph_search.py
+++ 02 - Ricerca 1/code/graph_search.py
@@ -11,7 +11,13 @@ graph = {'A': ['B', 'C'],
          'G': ['D', 'E', 'F']}
 
 # Each arc is represented as a tuple (node1, node2)
-graph2 = []
+graph2 =[('A','B') ,('A','C'),
+         ('B','A'), ('B','D'), ('B','E'),
+         ('C', 'A'), ('C','F'),
+		 ('D', 'B'), ('D','G'),
+         ('E', 'B'), ('E','G'),
+         ('F', 'C'), ('F','G'),
+         ('G', 'D'), ('G','E'), ('G','F')]
 
 class Agent():
     def __init__(self, graph, start, goal):
@@ -20,14 +26,27 @@ class Agent():
         self.frontier = [[start]]
 
     def next_states(self, path):
-        pass
+        #return [ n for n in graph[path[-1]] if n not in path ]
+        states = []
+        for n in graph[path[-1]]:
+            if n not in path:
+                states.append(n)
+        return states
 
     def is_goal(self, state):
-        pass
+        return state == self.goal
 
     def bfs(self):
-        #...
-        yield from self.bfs() 
+        if self.frontier == []:
+            yield []
+        else:
+            path = self.frontier.pop(0)
+            if self.is_goal(path[-1]):
+                yield path
+            # self.frontier += [path+[n] for n in self.next_states(path)]
+            for n in self.next_states(path):
+                self.frontier.append(path+[n])
+            yield from self.bfs()
 
 class Agent2(Agent):
     # graph as list of arcs, bfs iterative instead of recursive
@@ -35,19 +54,30 @@ class Agent2(Agent):
         super().__init__(graph, start, goal)
 
     def next_states(self, path):
-        pass
+        # return ( j for i,j in self.graph if i == path[-1] and j not in path )
+        states = []
+        for i,j in self.graph:
+            if i == path[-1] and j not in path:
+                states.append(j)
+        return states
+
 
     def bfs(self):
-        pass
+        while self.frontier:
+            path = self.frontier.pop(0)
+            if self.is_goal(path[-1]):
+                yield path
+            else:
+                self.frontier += [path+[n] for n in self.next_states(path)]
 
 
 
 if __name__ == "__main__":
-    edgelist = False
+    edgelist = True
     if edgelist:
-        a = Agent2(graph2, 'A', 'D')
+        a = Agent2(graph2, 'A', 'G')
     else:
-        a = Agent(graph, 'A', 'D')
+        a = Agent(graph, 'A', 'G')
     min_length = float('inf')
     for path in a.bfs():
         if len(path) <= min_length:
---
+++ 03 - Ricerca 2/code/solution/four_colors.py
@@ -0,0 +1,160 @@
+class DFSAgent:
+    def __init__(self, colors, adjacency):
+        self.colors = colors
+        self.adjacency = adjacency
+        self.state = {}
+        self.iters = 0
+
+    def valid_coloring(self, state, new_node=None):
+        for node in state if new_node is None else [new_node]:
+            for neighbor in self.adjacency[node]:
+                if neighbor in state and state[node] == state[neighbor]:
+                    return False
+        return True
+
+    def next_states(self, state):
+        return (
+            new_state
+            for node in self.adjacency if node not in state
+            for color in self.colors
+            if self.valid_coloring(new_state := {**state, node: color}, new_node=node)
+        )
+        #for node in self.adjacency:
+        #    if node not in state:
+        #        for color in self.colors:
+        #            new_state = state.copy()
+        #            new_state[node] = color
+        #            if self.valid_coloring(new_state):
+        #                yield new_state
+
+    def dfs(self):
+        self.iters += 1
+        if len(self.state) == len(self.adjacency):
+            print(self.iters)
+            yield self.state
+        for new_state in self.next_states(self.state):
+            self.state = new_state
+            yield from self.dfs()
+
+from itertools import chain
+class DFSAgentIterative(DFSAgent):
+    def __init__(self, colors, adjacency):
+        super().__init__(colors, adjacency)
+        self.frontier = (s for s in [{}])
+
+    def dfs(self):
+        while (state := next(self.frontier),None)!= None:
+            if len(state) == len(self.adjacency):
+                yield state
+            self.frontier = chain.from_iterable([self.next_states(state), self.frontier])
+
+class DFSAgentIterative2(DFSAgent):
+    def __init__(self, colors, adjacency):
+        super().__init__(colors, adjacency)
+        self.frontier = [{}, None]
+
+    def dfs(self):
+        while (state := self.frontier.pop(0))!= None:
+            if len(state) == len(self.adjacency):
+                print(self.generated_states)
+                yield state
+            self.frontier[0:0] = list(self.next_states(state)) 
+
+class DFSAgentOptim(DFSAgent):
+    def __init__(self, colors, adjacency, mrw=True, lcv=True, fwc=True):
+        super().__init__(colors, adjacency)
+        self.fwc, self.lcv, self.mrw = fwc, lcv, mrw
+
+    def legal_values(self, state, node):
+        return len(self.colors) - len({state[neighbor] for neighbor in self.adjacency[node] if neighbor in state})
+
+    def forward_checking(self, state):
+        return all(self.legal_values(state, node) > 0 for node in self.adjacency if node not in state) if self.fwc else True
+
+    def MCV_sort(self, state):
+        return min([n for n in self.adjacency if n not in state], key=lambda n: self.legal_values(state, n) if self.mrw else 0)
+
+    def removed_neighbors_colors(self, state, node, color):
+        neighbors = [ n for n in self.adjacency[node] if n not in state]
+        return sum(self.legal_values(state, n) for n in neighbors) - sum(self.legal_values({**state, node: color}, n) for n in neighbors)
+
+    def LCV_sort(self, state, node):
+        return sorted(self.colors, key=lambda c: self.removed_neighbors_colors(state, node, c) if self.lcv else 0)
+
+    def next_states(self, state):
+        node = self.MCV_sort(state)
+        return ( new_state 
+                for color in self.LCV_sort(state, node)
+                if self.valid_coloring(new_state := {**state, node: color}, new_node=node) and self.forward_checking(new_state))
+
+if __name__ == "__main__":
+    adjacency = {'A': ['B','G','F'],
+                'B': ['A','C'],
+                'C': ['B','D','F','E'],
+                'D': ['C'],
+                'E': ['C'],
+                'F': ['A','C','G'],
+                'G': ['A','F']}
+
+    #make a much larger graph
+    adjacency2 = {
+        1: [2, 4, 11, 13],
+        2: [1, 3, 10, 12],
+        3: [2, 6, 8, 11],
+        4: [1, 5, 8, 10, 14],
+        5: [4, 7, 9, 13],
+        6: [3, 7, 9, 12],
+        7: [5, 6, 8, 9, 14],
+        8: [3, 4, 7, 9, 12, 13],
+        9: [5, 6, 7, 8, 14],
+        10: [2, 4],
+        11: [1, 3],
+        12: [2, 6, 8],
+        13: [1, 5, 8],
+        14: [4, 7, 9],
+    }
+
+    adjacency3 = {
+        1: [2, 4, 11, 13],
+        2: [1, 3, 10, 12],
+        3: [2, 6, 8, 11, 15, 17],
+        4: [1, 5, 8, 10, 14, 17],
+        5: [4, 7, 9, 13, 16, 18],
+        6: [3, 7, 9, 12, 16, 18],
+        7: [5, 6, 8, 9, 14, 15, 17, 18],
+        8: [3, 4, 7, 9, 12, 13, 16, 18],
+        9: [5, 6, 7, 8, 14, 15, 16, 17],
+        10: [2, 4, 20, 22, 29],
+        11: [1, 3, 19, 21, 29],
+        12: [2, 6, 8, 20, 24, 26, 29],
+        13: [1, 5, 8, 19, 23, 26, 29],
+        14: [4, 7, 9, 22, 25, 27, 29],
+        15: [3, 7, 9, 21, 25, 27, 29],
+        16: [5, 6, 8, 9, 23, 24, 26, 27, 29],
+        17: [3, 4, 7, 9, 21, 22, 25, 27, 29],
+        18: [5, 6, 7, 8, 23, 24, 25, 26, 29],
+        19: [11, 13, 28, 30],
+        20: [10, 12, 28, 30],
+        21: [11, 15, 17, 28, 30],
+        22: [10, 14, 17, 28, 30],
+        23: [13, 16, 18, 28, 30],
+        24: [12, 16, 18, 28, 30],
+        25: [14, 15, 17, 18, 28, 30],
+        26: [12, 13, 16, 18, 28, 30],
+        27: [14, 15, 16, 17, 28, 30],
+        28: [19, 20, 21, 22, 23, 24, 25, 26, 27, 29, 30],
+        29: [10, 11, 12, 13, 14, 15, 16, 17, 18, 28, 30],
+        30: [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+    }
+
+
+    colors = ['red','green','blue','yellow']
+    a = DFSAgentOptim(colors, adjacency3)
+
+    for state in a.dfs():
+        print(state)
+        # ask user if they want to continue
+        print("Would you like to continue? (Y/n)")
+        answer = 'n' #input()
+        if answer == 'n':
+            break
