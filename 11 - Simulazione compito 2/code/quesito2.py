f800247a9fe3f2f693c54f65e3824458c87d8eac
Andrea-Augello <andrea.augello01community.unipa.it>
2025-01-13 12:44:03 +0100

    Corretto refuso

--- 11 - Simulazione compito 2/code/quesito2.py
+++ 11 - Simulazione compito 2/code/quesito2.py
@@ -16,7 +16,7 @@ cpd_low_pressure = TabularCPD(variable='Low Pressure', variable_card=2, values=[
 p_barometer_low_given_low_pressure = 0.7*0.4/0.3
 p_barometer_high_given_low_pressure = 1 - p_barometer_low_given_low_pressure
 cpd_barometer = TabularCPD(variable='Barometer', variable_card=2, evidence=['Low Pressure'], evidence_card=[2], values=[[0.1, p_barometer_low_given_low_pressure], [0.9, p_barometer_high_given_low_pressure]])
-cpd_red_sky = TabularCPD(variable='Red Sky', variable_card=2, evidence=['West Wind'], evidence_card=[2], values=[[0.6, 0.2], [0.4, 0.8]])
+cpd_red_sky = TabularCPD(variable='Red Sky', variable_card=2, evidence=['West Wind'], evidence_card=[2], values=[[0.2, 0.6], [0.8, 0.4]])
 
 cpd_rain = TabularCPD(variable='Rain', variable_card=2, evidence=['West Wind', 'Low Pressure'], evidence_card=[2, 2], values=[[0.8, 0.9, 0.3, 0.7], [0.2, 0.1, 0.7, 0.3]])
 
